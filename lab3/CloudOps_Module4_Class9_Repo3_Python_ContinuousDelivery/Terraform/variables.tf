84cc3f5074bdaeac24ca09a905707e650ba8ff5a
Xuanyuzhu <84358173+Xuanyuzhu@users.noreply.github.com>
2025-04-13 23:25:08 -0400

    update for lab3

--- lab3/CloudOps_Module4_Class9_Repo3_Python_ContinuousDelivery/Terraform/variables.tf
+++ lab3/CloudOps_Module4_Class9_Repo3_Python_ContinuousDelivery/Terraform/variables.tf
@@ -59,12 +59,12 @@ variable "security_group_name" {
 variable "ssh_key_private" {
   type    = string
   #Replace this with the location of you private key
-  default = "~/.ssh/id_ed25519"
+  default = "~/.ssh/id_rsa"
 }
 
 variable "ssh_key_public" {
   type    = string
   #Replace this with the location of you public key .pub
-  default = "~/.ssh/id_ed25519.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
--- lab3/CloudOps_Module4_Class9_Repo4_ProvisionAppServer/modules/compute/variables.tf
+++ lab3/CloudOps_Module4_Class9_Repo4_ProvisionAppServer/modules/compute/variables.tf
@@ -8,13 +8,13 @@ variable "region" {
 variable "ssh_key_public" {
   type    = string
   #Replace this with the location of you public key .pub
-  default = "~/.ssh/id_ed25519.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "ssh_key_private" {
   type    = string
   #Replace this with the location of you private key
-  default = "~/.ssh/id_ed25519"
+  default = "~/.ssh/id_rsa"
 }
 
 variable "subnet_ips" {}
